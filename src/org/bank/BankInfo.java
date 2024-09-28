32b70d7eb1dd18d4282ff2a40c84d6a1d04156cc
Bibin30 <bibinjoseraj@gmail.com>
2024-09-28 12:22:39 +0530

    new update

--- src/org/bank/BankInfo.java
+++ src/org/bank/BankInfo.java
@@ -14,6 +14,20 @@ public class BankInfo extends AxisBank {
 		b.savings();
 		b.fixed();
 		b.deposit();
+		
+		System.out.println(" ");
+		
+		System.out.println("New Bank updates");
+		System.out.println("----------------");
+		
+		System.out.println(" ");
+		
+		System.out.println("Karur Vysya Bank");
+		System.out.println("Tamilnadu Mercantile Bank");
+		System.out.println("City Union Bank");
+		System.out.println("Indian Overseas Bank");
+		System.out.println("Bank of Baroda");
+		
 	}
 
 }
