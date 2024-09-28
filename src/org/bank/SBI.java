5bd19e43cd60745b9759877db37309273204fe4a
Bibin30 <bibinjoseraj@gmail.com>
2024-09-28 12:04:52 +0530

    State Bank of India

---
+++ src/org/bank/SBI.java
@@ -0,0 +1,11 @@
+package org.bank;
+
+public class SBI {
+	
+	public static void main(String[] args) {
+		System.out.println("State bank of India");
+		System.out.println("SBI Life");
+		System.out.println("SBI Act 1934");
+	}
+
+}
