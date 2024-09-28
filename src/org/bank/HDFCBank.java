3bbcdc2a650e430004d6e84d01560a7099ca03f2
Bibin30 <bibinjoseraj@gmail.com>
2024-09-28 11:38:05 +0530

    Jose's code

---
+++ src/org/bank/HDFCBank.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class HDFCBank {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HDFC credit card");
+		System.out.println("HDFC EMI Finance");
+		System.out.println("HDFC Housing loan");
+		System.out.println("HDFC Gold Loan");
+		
+	}
+
+}
